258407e5419dfb91e52f3bbdcbb77932802af12e
Leandro Melo <leo1309melo@hotmail.com>
2024-10-03 23:08:51 -0300

    wip: adding components and setup project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'produto',
+    component: HomeComponent,
+  },
+  { path: '**', component: PageNotFoundComponent },
+];
